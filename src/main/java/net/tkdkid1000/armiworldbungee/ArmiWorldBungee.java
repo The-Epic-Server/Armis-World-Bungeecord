d13b9f8298644ac02953a1150329f4aedd9e9df6
TKDKid1000 <rhoneeiler@gmail.com>
2021-04-11 12:06:50 -0700

    added plugin variable

--- src/main/java/net/tkdkid1000/armiworldbungee/ArmiWorldBungee.java
+++ src/main/java/net/tkdkid1000/armiworldbungee/ArmiWorldBungee.java
@@ -1,9 +1,12 @@
 package net.tkdkid1000.armiworldbungee;
 
 import net.md_5.bungee.api.plugin.Plugin;
+import net.md_5.bungee.config.Configuration;
 
 public class ArmiWorldBungee extends Plugin {
 
+	Configuration config;
+	Plugin plugin = this;
 	@Override
 	public void onEnable() {
 		
