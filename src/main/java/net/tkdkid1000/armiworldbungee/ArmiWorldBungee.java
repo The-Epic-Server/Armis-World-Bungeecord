2e9dc080e111b417f9e0161f9e61e516ce9dfaac
TKDKid1000 <rhoneeiler@gmail.com>
2021-04-11 12:02:09 -0700

    initial commit

---
+++ src/main/java/net/tkdkid1000/armiworldbungee/ArmiWorldBungee.java
@@ -0,0 +1,11 @@
+package net.tkdkid1000.armiworldbungee;
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+public class ArmiWorldBungee extends Plugin {
+
+	@Override
+	public void onEnable() {
+		
+	}
+}
